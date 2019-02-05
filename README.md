712ffcb35af24e6006ee4d431908a47b95a2aad6
unknown <dariusz.nowotczynski@gmail.com>
2019-02-05 09:30:17 +0100

    Customize spreadsheet columns

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Download customized market data for further analysis in one spreadsheet in .csv 
 Select parameters of the final spreadsheet:
 - list of market instruments (by ticker)
 - date range
+- spreadsheet columns ('Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Open Interest')
 - data merging method ("outer" vs "inner")
 - data sorting by date method ("ascending" vs "descending")
 - file format (.csv or .xlsx)
--- downloader.py
+++ downloader.py
@@ -8,15 +8,27 @@ import os
 #INSTRUMENT LIST by ticker - list of instruments to download (Stooq.pl)
 ticker = [
     'KGH',
-    'CDR',
     'DNP',
     'WIG20',
-    'MWIG40'
+    'FW40',
+    'EURPLN'
 ]
 
 #DATE - date range in a format YYYY-MM-DD (default: 2018-01-02 to 2019-01-31)
 start_date = '2018-01-02'
 end_date = '2019-01-31'
+
+#DATA - type of data to be downloaded ('Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Open Interest')
+# each column can be customized, except column 'Data'
+data_columns = [
+    'Data',
+    'Otwarcie',
+    'Najwyzszy',
+    'Najnizszy',
+    'Zamkniecie',
+    'Wolumen',
+    'LOP'
+    ]
 
 #MERGE - method of merging data by date ('inner' or 'outer', default='outer')
 merge_type = 'outer'
@@ -25,7 +37,7 @@ merge_type = 'outer'
 sort_ascending = True
 
 #FILE FORMAT - type of file format ('csv' or 'xlsx', default='csv')
-file_format = 'xlsx'
+file_format = 'csv'
 
 #PATH - complete path where file with market data will be saved on your local computer;
 # if directory doesn't exist, it'll be created (default='C:\market_data')
@@ -40,8 +52,15 @@ file_name = '\market_data'
 
 # download data for each instrument from Stooq.pl and store in a list
 df_list = []
+columns = ['Data']
 for t in ticker:
     df = pd.read_csv('https://stooq.pl/q/d/l/?s=' + t + '&i=d')
+    for c in df.columns:
+        if c not in data_columns:
+            df.drop(c, axis=1, inplace=True)
+    for c in data_columns[1:]:
+        if c in df.columns:
+            columns.append(c+'_'+t)
     df_list.append(df)
 
 # merge all data into one spreadsheet
@@ -50,16 +69,13 @@ for i in range(1,len(df_list)):
     merged = pd.merge(merged,df_list[i], on='Data', how=merge_type)
 
 # create final names of columns
-columns = ['Date']
-for t in ticker:
-    columns.extend([t+'_Open',t+'_High',t+'_Low',t+'_Close',t+'_Volume'])
 merged.columns = columns
 
 # select data within a specified date range and sort appropriately (ascending or descending)
-merged['Date'] = pd.to_datetime(merged['Date'])
-merged = merged.set_index('Date')
+merged['Data'] = pd.to_datetime(merged['Data'])
+merged = merged.set_index('Data')
 merged = merged[start_date:end_date]
-merged.sort_values(by='Date', ascending=sort_ascending, inplace=True)
+merged.sort_values(by='Data', ascending=sort_ascending, inplace=True)
 
 # write data to csv or xlsx format and save on your local computer in a specified path
 if file_format == 'csv':
